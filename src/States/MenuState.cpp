d2081d73c0351ce951321cc92b392da5287b1288
Bezifabr <bezifabr@gmail.com>
2018-08-20 17:26:58 +0200

    Created TextButton class based on Entity

--- src/States/MenuState.cpp
+++ src/States/MenuState.cpp
@@ -4,8 +4,9 @@
 #include <iostream>
 #include "Components/SpriteComponent.h"
 #include "Components/TextComponent.h"
+#include "Components/ClickableComponent.h"
 
-#include "HUD/EntityButton.h"
+#include "HUD/TextButton.h"
 
 using std::cout;
 using std::endl;
@@ -57,10 +58,18 @@ void MenuState::OnLoad()
 	load->GetComponent<SpriteComponent>().sprite.setPosition(350, 300);
 	load->AddComponent<TextComponent>("Load", &textFont, sf::Vector2f(350,300));
 
-	// builder vs factory vs prototype
 	std::shared_ptr<HUD::EntityButton> testBtn(new HUD::EntityButton("TEST", "resources/btnText.png"));
 	testBtn->SetPosition(100, 100);
 	container.Add(testBtn);
+
+	std::shared_ptr<HUD::TextButton> testTextBtn(new HUD::TextButton("TEXTTEST", "resources/btnText.png"));
+	auto id = testTextBtn->GetId();
+	cout << id << endl;
+	testTextBtn->SetPosition(500, 500);
+	testTextBtn->SetFont(&textFont);
+	testTextBtn->SetString("TEXT");
+	testTextBtn->SetTextToCenter();
+	container.Add(testTextBtn);
 
 	cout << "Menu loaded" << endl;
 }
@@ -86,6 +95,10 @@ void MenuState::OnUpdate()
 		transition->Switch(std::shared_ptr<State>(new GameState));
 	}
 
+	if (container.Get(2).lock()->GetComponent<ClickableComponent>().IsReleased() == true)
+	{
+		isGameFinished = true;
+	}
 }
 
 void MenuState::OnHandleEvent()
@@ -100,6 +113,8 @@ void MenuState::OnHandleEvent()
 //		btnLoad.HandleEvent(event, mousePos);
 		btnExit.HandleEvent(event, mousePos);
 		btnPlay.HandleEvent(event, mousePos);
+
+		container.Get(2).lock()->GetComponent<ClickableComponent>().CheckIfIsClicked(event, (sf::Vector2i)mousePos);
 
 }
 
