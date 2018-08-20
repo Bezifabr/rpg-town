a00090889b021c7adc01e1b98c8905d70ee5dbe6
bezifabr <bezifabr@gmail.com>
2018-08-20 16:53:31 +0200

    Fixed ClickableComponent functionality

--- src/Components/ClickableComponent.cpp
+++ src/Components/ClickableComponent.cpp
@@ -6,19 +6,29 @@ ClickableComponent::ClickableComponent(sf::FloatRect clickableArea, bool isClick
 
 }
 
-void ClickableComponent::Refresh()
+ void ClickableComponent::Refresh()
 {
-    isPressed = false;
-    isReleased = false;
+       isReleased = false;
 }
 
 void ClickableComponent::CheckIfIsClicked(sf::Event event, sf::Vector2i mousePosition)
 {
     if(clickableArea.contains(sf::Vector2f(mousePosition.x, mousePosition.y)))
     {
-        if(event.type == sf::Event::MouseButtonReleased)    {isReleased = true; isPressed = false;}
-        if(event.type == sf::Event::MouseButtonPressed)     {isPressed = true; isReleased = false;}
+        if(event.type == sf::Event::MouseButtonReleased)    
+		{
+			if(isPressed == true) 
+				isReleased = true; 
+			isPressed = false;
+		}
+		if (event.type == sf::Event::MouseButtonPressed)
+		{
+			isPressed = true;
+			isReleased = false;
+		}
     }
+	else
+		isPressed = false;
 }
 
 bool ClickableComponent::IsPressed()
